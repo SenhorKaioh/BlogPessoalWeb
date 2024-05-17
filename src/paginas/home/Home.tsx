fdfcf4c56599b88c8e2d68ba83ef68dfb0aa90f5
senhorkaioh <caiopsantos.dev@gmail.com>
2024-05-17 11:39:42 -0300

    inicial commit

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -7,22 +7,17 @@ return(
         <div className=" gap-4 flex flex-col items-center  justify-center py-4">
           <h2 className=" text-5xl font-bold">Seja Bem Vindo!</h2>
           <p className="text-xl">Expresse aqui seus pensamentos e opniões</p>
+          <div className="rounded text-white border-white  bg-indigo-400
+            border-solid border-2 py-2 px-4">
+              Nova postagem
+            </div>
         </div>
         <div className="flex ">
           <img src="https://imgur.com/VpwApCU.png"
           alt="Imagem da Página da Home" 
           width="400px"
           />
-        </div>
-
-        <div className=" flex justify-around gap-4">
-            <div className="rounded text-white border-white 
-            border-solid border-2 py-2 px-4">
-              Nova postagem
-            </div>
-        </div>
-
-        
+        </div>   
      </div>
   </div>
   </>

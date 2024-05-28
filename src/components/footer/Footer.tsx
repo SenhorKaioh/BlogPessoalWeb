47b7556b6bac6418e5b27aae56fef1f2ea31b5bb
senhorkaioh <caiopsantos.dev@gmail.com>
2024-05-28 14:11:34 -0300

    feat: adição do perfil

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,17 +1,23 @@
 import { GithubLogo } from "@phosphor-icons/react/dist/icons/GithubLogo";
 import { InstagramLogo } from "@phosphor-icons/react/dist/icons/InstagramLogo";
 import { LinkedinLogo } from "@phosphor-icons/react/dist/icons/LinkedinLogo";
+import { ReactNode, useContext } from "react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../../contexts/AuthContext";
 
 
 function Footer(){
 
 let data = new Date().getFullYear()
 
-let component
+const {usuario} = useContext(AuthContext)
 
-    return(
-<>
+let component: ReactNode
+
+if (usuario.token !== ""){
+
+component = (
+
 <footer className="bg-indigo-900 text-white py-6">
   <div className="container flex flex-col items-center py-4">
   <p className='text-xl font-bold'>Blog Pessoal Caio |  Copyright: {data} </p>
@@ -42,6 +48,15 @@ let component
             </div>
       </div>
     </footer>
+
+)
+
+}
+
+
+    return(
+<>
+   {component}
         </>
 );
 }

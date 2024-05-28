1032ecd9e281b6f59813867136c891b4e987607a
senhorkaioh <caiopsantos.dev@gmail.com>
2024-05-28 13:44:14 -0300

    fix: deletar e atualizar postagens

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -46,7 +46,7 @@ function Navbar(){
                             <Link to='/postagens' className='hover:underline'>Postagens</Link>
                             <Link to='/temas' className='hover:underline'>Temas</Link>
                             <Link to='/cadastrartema' className='hover:underline'>Cadastrar tema</Link>
-                            Perfil
+                            <Link to='/perfil' className='hover:underline'>Perfil</Link>
     
                             {/* 
                                 Envolvemos o texto Sair com o Componente Link e adicionamos 
